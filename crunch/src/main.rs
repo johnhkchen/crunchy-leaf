f1d5fc221419c7ecf917f609994cdee0fcfddb5f
John Chen <john.hk.chen@gmail.com>
2022-11-15 15:17:44 -0800

    The first test!

--- crunch/src/main.rs
+++ crunch/src/main.rs
@@ -1,3 +1,24 @@
+fn foo() -> usize{
+    return 42;
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", foo());
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_knows() {
+        use crate::foo;
+        let result = foo();
+        assert_eq!(result, 42);
+    }
 }
